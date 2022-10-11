51548c14b9d39b45730af59e0c3623feedeb25c9
Chris Hamilton <chris@tracktownsoftware.com>
2022-10-11 16:14:14 -0700

    latest changes

---
+++ WpfCustomControlLibrary/WpfCustomControlLibrary.DesignTools/CustomControl1DefaultInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.DesignTools.Extensibility.Model;
+
+namespace WpfCustomControlLibrary.DesignTools
+{
+    internal class CustomControl1DefaultInitializer : DefaultInitializer
+    {
+        public CustomControl1DefaultInitializer() 
+            : base()
+        {
+        }
+
+        public override void InitializeDefaults(ModelItem item)
+        {
+            item.Properties["Height"].SetValue("100");
+            item.Properties["Width"].SetValue("250");
+            item.Properties["Background"].SetValue("LightGreen");
+            base.InitializeDefaults(item);
+        }
+    }
+}
---
+++ WpfCustomControlLibrary/WpfCustomControlLibrary.DesignTools/RegisterMetaData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.DesignTools.Extensibility;
+using Microsoft.VisualStudio.DesignTools.Extensibility.Features;
+using Microsoft.VisualStudio.DesignTools.Extensibility.Metadata;
+
+// The ProvideMetadata assembly-level attribute indicates to designers
+// that this assembly contains a class that provides an attribute table. 
+[assembly: ProvideMetadata(typeof(WpfCustomControlLibrary.DesignTools.RegisterMetaData))]
+
+namespace WpfCustomControlLibrary.DesignTools
+{
+    public class RegisterMetaData : IProvideAttributeTable
+    {
+
+        public AttributeTable AttributeTable
+        {
+            get 
+            {
+                AttributeTableBuilder builder = new AttributeTableBuilder();
+
+                // Show controls in toolbox
+                builder.AddCustomAttributes("WpfCustomControlLibrary.CustomControl1", new ToolboxBrowsableAttribute(true));
+                //builder.AddCustomAttributes("WpfCustomControlLibrary.CustomControl2", new ToolboxBrowsableAttribute(false));
+
+                builder.AddCustomAttributes(
+                    "WpfCustomControlLibrary.CustomControl1",
+                    new FeatureAttribute(typeof(CustomControl1DefaultInitializer)));
+
+                // Add the FormulaBar default initializer to the design-time metadata.
+                builder.AddCustomAttributes(
+                    "WpfCustomControlLibrary.CustomControl2",
+                    new FeatureAttribute(typeof(CustomControl2DefaultInitializer)));
+
+                return builder.CreateTable();
+            }
+        }
+    }
+}
