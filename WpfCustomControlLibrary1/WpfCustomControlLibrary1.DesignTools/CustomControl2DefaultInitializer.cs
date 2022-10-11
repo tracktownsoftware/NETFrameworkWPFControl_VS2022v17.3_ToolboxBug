b52d97592e0afaff987e9cd4d7d0ff53546369ee
Chris Hamilton <chris@tracktownsoftware.com>
2022-10-11 14:09:59 -0700

    first pass

---
+++ WpfCustomControlLibrary1/WpfCustomControlLibrary1.DesignTools/CustomControl2DefaultInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.DesignTools.Extensibility.Model;
+
+namespace WpfCustomControlLibrary1.DesignTools
+{
+    internal class CustomControl2DefaultInitializer : DefaultInitializer
+    {
+        public CustomControl2DefaultInitializer()
+            : base()
+        {
+        }
+
+        public override void InitializeDefaults(ModelItem item)
+        {
+            item.Properties["Height"].SetValue("100");
+            item.Properties["Width"].SetValue("250");
+            item.Properties["Background"].SetValue("LightPink");
+            base.InitializeDefaults(item);
+        }
+    }
+}
